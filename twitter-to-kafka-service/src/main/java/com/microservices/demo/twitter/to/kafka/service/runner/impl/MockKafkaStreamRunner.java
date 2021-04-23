014df231404345afd9ad927e43a73edaa4893a1e
Yan <sheny308@yahoo.com>
2021-04-23 08:43:12 -0400

    Develop (#1)
    * set up twitter-to-kafka
    
    * commit 2
    
    * commit 3
    
    * commit 4
    
    Co-authored-by: YAN SHEN <yanshen@YANs-MacBook-Pro.local>

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/MockKafkaStreamRunner.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.twitter.to.kafka.service.runner.impl;
+
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
+import twitter4j.TwitterException;
+
+public class MockKafkaStreamRunner implements StreamRunner {
+    @Override
+    public void start() throws TwitterException {
+    // adding commnets
+        // commits 2 activity
+        // commit 3 activity
+        // commit 4 activity
+    }
+}
